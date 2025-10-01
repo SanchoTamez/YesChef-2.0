c188fdc494fd8f883b6cc5b75f81efcfff7b876a
Elan Tamez <tamezapple@gmail.com>
2025-10-01 14:21:52 -0600

    Initial Commit

---
+++ YesChef 2.0/YesChef_2_0App.swift
@@ -0,0 +1,17 @@
+//
+//  YesChef_2_0App.swift
+//  YesChef 2.0
+//
+//  Created by Tamez, Elan - Student on 10/1/25.
+//
+
+import SwiftUI
+
+@main
+struct YesChef_2_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ YesChef 2.0Tests/YesChef_2_0Tests.swift
@@ -0,0 +1,17 @@
+//
+//  YesChef_2_0Tests.swift
+//  YesChef 2.0Tests
+//
+//  Created by Tamez, Elan - Student on 10/1/25.
+//
+
+import Testing
+@testable import YesChef_2_0
+
+struct YesChef_2_0Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
